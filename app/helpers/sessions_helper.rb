c08af65ae46db473e47251c9964461d5e5fe4757
Brian Trickovic <me@briantrickovic.com>
2024-02-19 21:32:32 +0000

    Forget a permanently logged in user (enable full log out functionality)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,14 +4,14 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
-  
+
   # Remembers a user in a persistent session.
   def remember(user)
     user.remember
     cookies.permanent.encrypted[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
-  
+
   # Returns the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
@@ -24,14 +24,22 @@ module SessionsHelper
       end
     end
   end
-  
+
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
   end
-  
+
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # Logs out the current user.
   def log_out
+    forget(current_user)
     reset_session
     @current_user = nil
   end
